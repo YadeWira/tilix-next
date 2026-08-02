617621ee848e72c278a1a2d0ef4b0f9fb38e721e
YadeWira <sebasgamerelpro@gmail.com>
2026-08-01 22:43:00 -0500

    gtk4-port: reduce threads.d to thin wrappers over GLib
    GTK4 removed the gdk_threads_* family along with the GDK lock, so the
    idle/timeout helpers now call g_idle_add and g_timeout_add directly.
    
    Almost all of this module existed to work around gtk-d not accepting D
    delegates as GSourceFuncs: a DelegatePointer struct, a C-linkage trampoline,
    variadic parameter forwarding and manual GC.addRoot/removeRoot to keep the
    delegate alive across the callback. GID takes a D delegate for SourceFunc
    itself, so all of that goes: 193 lines down to 46, with the exception
    handling kept.
    
    The variadic parameter support is dropped because nothing used it — all
    five call sites pass a zero-argument delegate. Those five already return
    bool, so they match SourceFunc unchanged; the module keeps its old
    function names deliberately so the callers do not need touching.

--- source/gx/gtk/threads.d
+++ source/gx/gtk/threads.d
@@ -4,190 +4,43 @@
  */
 module gx.gtk.threads;
 
-import core.memory;
-
-import std.algorithm;
 import std.experimental.logger;
-import std.stdio;
 
-import gdk.Threads;
-
-/**
-  * Simple structure that contains a pointer to a delegate. This is necessary because delegates are not directly
-  * convertable to a simple pointer (which is needed to pass as data to a C callback).
-  *
-  * This code from grestful (https://github.com/Gert-dev/grestful)
-  */
-struct DelegatePointer(S, U...)
-{
-	S delegateInstance;
-
-	U parameters;
-
-	/**
-      * Constructor.
-      *
-      * @param delegateInstance The delegate to invoke.
-      * @param parameters       The parameters to pass to the delegate.
-      */
-	public this(S delegateInstance, U parameters)
-	{
-		this.delegateInstance = delegateInstance;
-		this.parameters = parameters;
-	}
-}
+import glib.global : idleAdd, timeoutAdd;
+import glib.types : PRIORITY_DEFAULT, PRIORITY_DEFAULT_IDLE, SourceFunc;
 
 /**
-  * Callback that will invoke the passed DelegatePointer's delegate when it is called. This very useful method can be
-  * used to pass delegates to gdk.Threads.threadsAddIdle instead of having to define a callback with C linkage and a
-  * different method for every different action.
-  *
-  * The return type is the type that should be returned by this function. The invoked delegate should as a best practice
-  * return the same value. If an exception happens and the value from the delegate can't be returned, the '.init' value
-  * of the type will be used instead (or nothing in the case of void).
-  *
-  * Finally, if doRemoveRoot is set to true, this function will execute a removeRoot on the garbage collector for the
-  * passed data (which is the delegate). This is useful in situations where you're passing a delegate to a C function
-  * that will happen asynchronously, in which case you should be adding the newly allocated DelegatePointer using
-  * addRoot to ensure the garbage collector doesn't attempt to collect the delegate while the callback hasn't been
-  * invoked yet.
-  *
-  * @param data The data that is passed to the method.
-  *
-  * @return Whether or not the method should continue executing.
-  *
-  * This code from grestful (https://github.com/Gert-dev/grestful)
-  */
-extern(C) nothrow static ReturnType invokeDelegatePointerFunc(S, ReturnType, bool doRemoveRoot = false)(void* data)
-{
-	auto callbackPointer = cast(S*) data;
-
-	try
-	{
-		static if (__traits(compiles, ReturnType.init))
-		{
-			auto returnValue = callbackPointer.delegateInstance(callbackPointer.parameters);
-			return returnValue;
-		}
-
-		else
-		{
-			callbackPointer.delegateInstance(callbackPointer.parameters);
-		}
-	}
-
-	catch (Exception e)
-	{
-		// Just catch it, can't throw D exceptions accross C boundaries.
-		static if (__traits(compiles, ReturnType.init))
-			return ReturnType.init;
-	}
-
-	// Should only end up here for types that don't have an initial value (such as void).
-}
-
-/**
- * Convenience method that allows scheduling a delegate to be executed with gdk.Threads.threadsAddIdle instead of a
- * traditional callback with C linkage.
+ * Schedules a delegate to be run on the main loop once it is idle. The
+ * delegate returns whether it wants to be run again, exactly like a
+ * GSourceFunc.
  *
- * @param theDelegate The delegate to schedule.
- * @param parameters  A tuple of parameters to pass to the delegate when it is invoked.
- *
- * @example
- *     auto myMethod = delegate(string name, string value) { do_something_with_name_and_value(); }
- *     threadsAddIdleDelegate(myMethod, "thisIsAName", "thisIsAValue");
- *
- * This code from grestful (https://github.com/Gert-dev/grestful)
+ * GTK4 removed the gdk_threads_* family along with the GDK lock, so this now
+ * calls GLib directly. GID passes D delegates through to g_idle_add itself,
+ * which is why none of the DelegatePointer/GC.addRoot marshalling that the
+ * gtk-d version needed is here any more.
  */
-void threadsAddIdleDelegate(T, parameterTuple...)(T theDelegate, parameterTuple parameters)
-{
-	void* delegatePointer = null;
-
-	auto wrapperDelegate = (parameterTuple parameters) {
-		bool callAgainNextIdleCycle = false;
-
-		try
-		{
-			callAgainNextIdleCycle = theDelegate(parameters);
-			//if (callAgainNextIdleCycle) trace("Callback again is true");
-			//else trace("Callback again is false");
-		}
-
-		catch (Exception e)
-		{
-			warning("Unexpected exception occurred in wrapper");
-			// Catch exceptions here as otherwise, memory may never be freed below.
-		}
-
-		if (!callAgainNextIdleCycle) {
-			//trace("Removing delegate pointer");
-			GC.removeRoot(delegatePointer);
-			return false;
-		} else return true;
-	};
-
-	delegatePointer = cast(void*) new DelegatePointer!(T, parameterTuple)(wrapperDelegate, parameters);
-
-	// We're going into a separate thread and exiting here, make sure the garbage collector doesn't think the memory
-	// isn't used anymore and collects it.
-	GC.addRoot(delegatePointer);
-
-	gdk.Threads.threadsAddIdle(
-		cast(GSourceFunc) &invokeDelegatePointerFunc!(DelegatePointer!(T, parameterTuple), int),
-		delegatePointer
-		);
+void threadsAddIdleDelegate(SourceFunc theDelegate) {
+    idleAdd(PRIORITY_DEFAULT_IDLE, () {
+        try {
+            return theDelegate();
+        } catch (Exception e) {
+            warningf("Unexpected exception in idle callback: %s", e.msg);
+            return false;
+        }
+    });
 }
 
 /**
- * Convenience method that allows scheduling a delegate to be executed with gdk.Threads.threadsAddTimeout instead of a
- * traditional callback with C linkage.
- *
- * @param interval The interval to call the delegate in ms
- * @param theDelegate The delegate to schedule.
- * @param parameters  A tuple of parameters to pass to the delegate when it is invoked.
- *
- * @example
- *     auto myMethod = delegate(string name, string value) { do_something_with_name_and_value(); }
- *     threadsAddIdleDelegate(myMethod, "thisIsAName", "thisIsAValue");
- *
- * This code from grestful (https://github.com/Gert-dev/grestful)
+ * Schedules a delegate to be run on the main loop every interval milliseconds
+ * until it returns false. Returns the source id so the caller can cancel it.
  */
-uint threadsAddTimeoutDelegate(T, parameterTuple...)(uint interval, T theDelegate, parameterTuple parameters)
-{
-	void* delegatePointer = null;
-
-	auto wrapperDelegate = (parameterTuple parameters) {
-		bool callAgainNextIdleCycle = false;
-
-		try
-		{
-			callAgainNextIdleCycle = theDelegate(parameters);
-			//if (callAgainNextIdleCycle) trace("Callback again is true");
-			//else trace("Callback again is false");
-		}
-
-		catch (Exception e)
-		{
-			warning("Unexpected exception occurred in wrapper");
-			// Catch exceptions here as otherwise, memory may never be freed below.
-		}
-
-		if (!callAgainNextIdleCycle) {
-			//trace("Removing delegate pointer");
-			GC.removeRoot(delegatePointer);
-			return false;
-		} else return true;
-	};
-
-	delegatePointer = cast(void*) new DelegatePointer!(T, parameterTuple)(wrapperDelegate, parameters);
-
-	// We're going into a separate thread and exiting here, make sure the garbage collector doesn't think the memory
-	// isn't used anymore and collects it.
-	GC.addRoot(delegatePointer);
-
-	return gdk.Threads.threadsAddTimeout(
-		interval,
-		cast(GSourceFunc) &invokeDelegatePointerFunc!(DelegatePointer!(T, parameterTuple), int),
-		delegatePointer
-		);
+uint threadsAddTimeoutDelegate(uint interval, SourceFunc theDelegate) {
+    return timeoutAdd(PRIORITY_DEFAULT, interval, () {
+        try {
+            return theDelegate();
+        } catch (Exception e) {
+            warningf("Unexpected exception in timeout callback: %s", e.msg);
+            return false;
+        }
+    });
 }
